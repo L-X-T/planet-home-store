656c24f65ad60fefe6033c99897e20e6ac2b8f0b
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2024-10-28 10:14:09 +0100

    [ngrx] add effect

--- apps/flight-app/src/app/flight-booking/+state/flight-booking.actions.ts
+++ apps/flight-app/src/app/flight-booking/+state/flight-booking.actions.ts
@@ -4,6 +4,7 @@ import { Flight } from '@flight-workspace/flight-lib';
 export const FlightBookingActions = createActionGroup({
   source: 'FlightBooking',
   events: {
+    loadFlights: props<{ from: string; to: string; urgent: boolean }>(),
     loadFlightsSuccess: props<{ flights: Flight[] }>(),
     updateFlight: props<{ flight: Flight }>()
   }
