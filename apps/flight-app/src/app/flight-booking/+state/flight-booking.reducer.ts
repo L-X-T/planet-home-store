1bd7c33a98832c1730bfbadd9d4a26fcd9022dbf
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2024-10-29 13:44:33 +0100

    Add recucer (long options)

--- apps/flight-app/src/app/flight-booking/+state/flight-booking.reducer.ts
+++ apps/flight-app/src/app/flight-booking/+state/flight-booking.reducer.ts
@@ -1,6 +1,7 @@
 import { createReducer, on } from '@ngrx/store';
 import { FlightBookingActions } from './flight-booking.actions';
 import { FlightBookingState, initialState } from './flight-booking.state';
+import { Flight } from '@flight-workspace/flight-lib';
 
 export const flightBookingFeatureKey = 'flightBooking';
 
@@ -21,10 +22,36 @@ export const reducer = createReducer(
     (state, { err }): FlightBookingState => ({ ...state, flights: [], isLoadingFlights: false, flightsLoadError: err })
   ),
 
+  /*on(FlightBookingActions.loadFlightsSuccess, (state, action): FlightBookingState => {
+    const flights: Flight[] = action.flights;
+    return {
+      ...state,
+      flights: flights,
+      isLoadingFlights: false,
+      flightsLoadError: null
+    };
+  }),*/
+
+  /*on(
+    FlightBookingActions.loadFlightsSuccess,
+    (state, action): FlightBookingState => ({
+      ...state,
+      flights: action.flights,
+      isLoadingFlights: false,
+      flightsLoadError: null
+    })
+  ),*/
+
   on(
     FlightBookingActions.loadFlightsSuccess,
     (state, { flights }): FlightBookingState => ({ ...state, flights, isLoadingFlights: false, flightsLoadError: null })
   ),
+
+  on(FlightBookingActions.updateFlight, (state, action): FlightBookingState => {
+    const flight = action.flight;
+    const flights = state.flights.map((f) => (f.id === flight.id ? flight : f));
+    return { ...state, flights: flights };
+  }),
 
   on(FlightBookingActions.updateFlight, (state, action): FlightBookingState => {
     const flight = action.flight;
