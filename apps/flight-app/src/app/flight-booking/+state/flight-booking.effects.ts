0359fe156d3438c3262d54aba3c7ddbd3469015a
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2024-10-31 09:28:37 +0100

    [ngrx] debug store

--- apps/flight-app/src/app/flight-booking/+state/flight-booking.effects.ts
+++ apps/flight-app/src/app/flight-booking/+state/flight-booking.effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, map, of, switchMap } from 'rxjs';
+import { catchError, map, of, switchMap, tap } from 'rxjs';
 import { FlightService } from '@flight-workspace/flight-lib';
 import { FlightBookingActions } from './flight-booking.actions';
 
@@ -9,12 +9,16 @@ export class FlightBookingEffects {
   loadFlights$ = createEffect(() =>
     this.actions$.pipe(
       ofType(FlightBookingActions.loadFlights),
-      switchMap((action) =>
-        this.flightService.find(action.from, action.to, action.urgent).pipe(
+      switchMap((action) => {
+        // step 1: check action
+        console.log('[effect] action:', action);
+        return this.flightService.find(action.from, action.to, action.urgent).pipe(
+          // step 2: check api result
+          tap((result) => console.log('[effect] api result:', result)),
           map((flights) => FlightBookingActions.loadFlightsSuccess({ flights })),
           catchError((err) => of(FlightBookingActions.loadFlightsFailure({ err })))
-        )
-      )
+        );
+      })
     )
   );
 
